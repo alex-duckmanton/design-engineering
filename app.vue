ed156eab452a205fc7398a65b9497e1b4eb15cce
Alex Duckmanton <alex.duckmanton@relevanceai.com>
2025-12-10 17:57:55 +1100

    Add the tutorial

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const states = ['state-1', 'state-2', 'state-3']
+const currentState = ref('')
+
+function randomizeState() {
+  var newVal = states[Math.floor(Math.random() * states.length)]
+
+  if (newVal === currentState.value) newVal = false
+
+  currentState.value = newVal
+}
+</script>
+
 <template>
-  <div>
-    <p>hi</p>
+  <div class="container">
+    <div :class="['blob', currentState]"></div>
+    <button @click="randomizeState">I'm a button</button>
   </div>
 </template>
+
+<style>
+@import "~/assets/css/main.css";
+</style>
+
